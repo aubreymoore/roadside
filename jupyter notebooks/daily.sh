8d0b50d01f779a33b59d73d9d73cd3cd4ade4003
Aubrey Moore <aubreymoore@guam.net>
2020-07-12 11:03:08 +1000

    added boundin boxes notebook

---
+++ jupyter notebooks/daily.sh
@@ -0,0 +1,2 @@
+#papermill georef.ipynb /media/aubrey/9C33-6BBD/20200706/georef.ipynb -p DATAPATH /media/aubrey/9C33-6BBD -p DATE 20200706
+papermill routemap.ipynb /media/aubrey/9C33-6BBD/20200706/routemap.ipynb -p DATAPATH /media/aubrey/9C33-6BBD -p DATE 20200706
