769522000ad8c308a95682e5a96f450c2381b193
Aubrey Moore <aubreymoore@guam.net>
2020-07-20 14:54:27 +1000

    minor changes

---
+++ jupyter notebooks/get-random-bounding-boxes/doit.sh
@@ -0,0 +1,13 @@
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command generated exit code $?."' EXIT
+
+NOTEBOOK="reports/job27.ipynb"
+papermill --prepare-only get-random-bounding-boxes.ipynb $NOTEBOOK -y '{"NSAMPLES":"10"}' 
+jupyter nbconvert --execute --to html $NOTEBOOK
+
